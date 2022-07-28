56905c602d0d378cf7ddda672db9a528f7c0b962
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-29 00:53:25 +0600

    calculate total price, shipping charge, tax

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,18 +1,27 @@
 import React from 'react';
-// import './Cart.css'
+import './Cart.css';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Cart = ({ cart }) => {
+    // console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        console.log(product);
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
     return (
-        <div>
+        <div className='cart'>
             <h5>Our Summary</h5>
             <div className='order-details'>
                 <p>Selected items: {cart.length}</p>
-                <p>Total Price: $</p>
-                <p>Total Shipping Charge: $</p>
-                <p>Tax: $</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: ${tax}</p>
                 <h6>Grand Total: $</h6>
             </div>
             <div className='btns'>
