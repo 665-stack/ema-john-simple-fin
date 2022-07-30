35519225631e28618444824c0cb39c6e76dcc140
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-30 13:56:08 +0600

    recap code

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -9,7 +9,6 @@ const Cart = ({ cart }) => {
     let shipping = 0;
     let quantity = 0;
     for (const product of cart) {
-        console.log(product);
         quantity = quantity + product.quantity;
         total = total + product.price * product.quantity;
         shipping = shipping + product.shipping * product.quantity;
