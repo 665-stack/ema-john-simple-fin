b5fb08560b7c041c5363878057d60525a154a0c1
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-17 13:34:05 +0600

    create a form in login component and add style

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h2>login</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+
+                <form className='form' action="">
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="Password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p >New to Ema-John? <Link className='form-link' to="/signUp">Create an account</Link></p>
+            </div>
         </div>
     );
 };
