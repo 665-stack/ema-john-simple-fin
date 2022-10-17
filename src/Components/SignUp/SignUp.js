5c798265ae5fe88b515da34cbfd6b669a88ab7d3
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-17 23:06:39 +0600

    add form in sign up component

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 const SignUp = () => {
     return (
-        <div>
-            <h2>signup</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+
+                <form className='form' action="">
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" name="confirem-password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p className='form-text'>Already have an account? <Link className='form-link' to="/login"> Login </Link></p>
+
+                <div className="form-or-line">
+                    <div></div>
+                    <span>or</span>
+                    <div></div>
+                </div>
+            </div>
         </div>
     );
 };
