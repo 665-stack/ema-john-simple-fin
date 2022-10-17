fda762b4f3c4026f7653a03e68462ad2add94e9a
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-18 02:30:17 +0600

    install react firebase hook and implement for create user

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './SignUp.css'
+import auth from '../../firebase.init';
+
 const SignUp = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const navigate = useNavigate()
+
+    const [
+        createUserWithEmailAndPassword,
+        user
+    ] = useCreateUserWithEmailAndPassword(auth);
+
 
     const handleEmailBlur = event => {
         setEmail(event.target.value)
@@ -16,13 +27,30 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value)
     }
+
+    if (user) {
+        console.log(user)
+        navigate('/')
+    }
+
+
     const handleCreateUser = event => {
         event.preventDefault()
         if (password !== confirmPassword) {
             setError('You two password did not match')
             return;
         }
+        if (password.length < 6) {
+            setError('password must be 6 characters or longer')
+            return;
+        }
+        setError('')
+        setSuccess('User created')
+        console.log('user created :)')
+
+        createUserWithEmailAndPassword(email, password);
     }
+
 
     return (
         <div className='form-container'>
@@ -42,7 +70,8 @@ const SignUp = () => {
                         <label htmlFor="confirm-password">Confirm Password</label>
                         <input onBlur={handleConfirmPasswordBlur} type="password" name="confirem-password" id="" required />
                     </div>
-                    <p className='form-error'></p>
+                    <p className='form-error'>{error}</p>
+                    <p className='form-success'>{success}</p>
                     <input className='form-submit' type="submit" value="Sign Up" />
                 </form>
                 <p className='form-text'>Already have an account? <Link className='form-link' to="/login"> Login </Link></p>
